f145c7d7c781f7cc0eb56fd4d88f71c387bd339c
EunhaLim <dmsgk1559@gmail.com>
2022-08-24 15:32:25 +0900

    useContext instead of Consumer in ColorBox component

--- React/day15_context_api/components/ColorBox.js
+++ React/day15_context_api/components/ColorBox.js
@@ -1,23 +1,45 @@
-import { ColorConsumer } from '../contexts/color';
+// import { ColorConsumer } from '../contexts/color';
+import { useContext } from 'react';
+import ColorContext from '../contexts/color';
 
 const ColorBox = () => {
+  /* use useContext Hook */
+  const { state } = useContext(ColorContext);
+
   return (
-    <ColorConsumer>
-      {({ state }) => (
-        <>
-          <div style={{
-            width: '80px',
-            height: '80px',
-            background: state.color
-          }}></div>
-          <div style={{
-          width: '50px',
-          height: '50px',
-          background: state.subcolor
-        }}></div>
-        </>
-      )}
-    </ColorConsumer>
+    /* use Consumer */
+    // <ColorConsumer>
+    //   {({ state }) => (
+    //     <>
+    //       <div style={{
+    //         width: '80px',
+    //         height: '80px',
+    //         background: state.color
+    //       }}></div>
+    //       <div style={{
+    //         width: '50px',
+    //         height: '50px',
+    //         background: state.subcolor
+    //       }}></div>
+    //     </>
+    //   )}
+    // </ColorConsumer>
+
+    /* use useContext Hook */
+    <>
+      <div style={{
+        width: '80px',
+        height: '80px',
+        background: state.color
+      }}
+      ></div>
+      <div style={{
+        width: '50px',
+        height: '50px',
+        background: state.subcolor
+      }}
+      ></div>
+    </>
   );
 };
 export default ColorBox;
