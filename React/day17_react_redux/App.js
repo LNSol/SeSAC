0ee59db4a6106857d32de97ecba7a18f7f896732
EunhaLim <dmsgk1559@gmail.com>
2022-08-25 18:30:46 +0900

    react redux tutorial 1.....

---
+++ React/day17_react_redux/App.js
@@ -0,0 +1,14 @@
+import Counter from './components/Counter';
+import CounterContainer from './containers/CounterContainer';
+import Todos from './components/Todos';
+
+const App = () => {
+  return (
+    <div>
+      <CounterContainer />
+      <hr />
+      <Todos />
+    </div>
+  );
+}
+export default App;
